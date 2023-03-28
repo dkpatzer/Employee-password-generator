536bcf08c7eeda51f25ded01accdf9b9410ef701
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-03-27 23:18:44 -0500

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
