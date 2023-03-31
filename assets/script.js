195857e25399f8351086b818f1901b34d8e1ebaa
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-03-31 16:57:10 -0500

    pseudocode outline added

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
 // Assignment Code
+// Define a function: generatePassword
+// inside the function declare and intitate variables to include:
+//    1. Length of password between 8 and 128 characters
+//    2.define the characters to be used in the password-upper and lower case letters, numbers, and special characters
+//    3.an empty string to store the generated password
+
+// User is prompted to select lenth and then which of the three possible variables for 
+// to include in the password
+
+// A loop is created to generate a random character from the possible characters chosen
+// and to append it onto the password string until the string reaches the desired length
+
+// Return the generated password
+
+// Call the generatePassword password function when the generatePassword password button is clicked and display it on the screen
+
+
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   
