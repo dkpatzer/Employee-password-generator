ae0392a51db08c1fb0cc86a6e6d64d8e13bcb40d
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-04-02 23:47:46 -0500

    Final code and readme files

---
+++ Develop/script.js
@@ -0,0 +1,77 @@
+/* Assignment Code */
+
+/* assign a button with id "generate to the variable "generateBtn"*/
+/*Define a function: generatePassword*/
+
+
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  
+}
+/* write function to be called when "Generate Password Button is clicked"*/
+/*input field with id "pasword" password is assigned to the value property*/
+/* Write password to the #password input*/
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+/*generate password function includes variables to prompt user to enter chosen length and variables to confirm chosen characters*/
+
+
+function generatePassword() {
+  var length = prompt("Enter password length (8-128)");
+  var lowercase = confirm("Include lowercase characters?");
+  var uppercase = confirm("Include uppercase characters?");
+  var numeric = confirm("Include numeric characters?");
+  var special = confirm("Include special characters?");
+
+    /*Validate user input*/
+  if (length < 8 || length > 128) {
+      alert("Password length must be between 8 and 128 characters");
+      return "";
+  }
+
+  if (!lowercase && !uppercase && !numeric && !special) {
+      alert("At least one character type must be selected");
+      return "";
+  }
+  /* create an array of character codes based on selected character types*/
+  /*create a loop selecting random characters from the array until it gets to the last number in the lenghth chosen*/
+  var charCodes = [];
+  if (lowercase) {
+    for (var i = 97; i <= 122; i++) {
+      charCodes.push(i);
+    }
+  }
+  if (uppercase) {
+    for (var i = 65; i <= 90; i++) {
+      charCodes.push(i);
+    }
+  }
+
+  if (special) {
+    var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    for (var i = 0; i < specialChars.length; i++) {
+      charCodes.push(specialChars.charCodeAt(i));
+    }
+  }
+ 
+  /* random password is generated using math.random() method*/
+
+  var passwordArr = [];
+  for (var i = 0; i < length; i++) {
+    var randomCharCode = charCodes[Math.floor(Math.random() * charCodes.length)];
+    passwordArr.push(String.fromCharCode(randomCharCode));
+  }
+/*random password is returned to empty string*/
+
+  var password = passwordArr.join("");
+  return password;
+}
+
+/* Add event listener to generate button which calls the writePassword function when the button is clicked*/
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Employee-password-generator
+Description:
+This is a password generator application  built to provide a secure password for an employee with access to sensitive information. It generates a random passord based on the user's input. It has been developed using HTML, CSS, and Javascript.
+
+Instructions for Use
+1. Click the "Generate Password" button.
+2. Enter the desired length of the password.
+3. Confirm which character types to use in the password. Choices include uppercase, lowercase, numeric, and/or special characters in the prompts.
+4. The password generated will display in the input field field with the id password
+
+Discussion of JavaScript code used:
+
+generatePassword() declares a function that ultimately generates a user password based on the user's input of length and characters. It validates the users input and creates an array of character codes.It finally generates a random password by selecting characters from the array and returns the password as a string.
+
+writePassword() funtion is triggered when the generate password button is clicked. It calls generatePassword to generate a password and displays it with the id "password".
+
+The event listener listens for clicks on the Generate Password button and calls the writePassword function.
+
+Credits:
+
+The password generator was developed by David Patzer as part of the NU coding bootcamp. I obtained information from many sources to help put this together. This includes w3collective, MDN understanding Math.random(), Stack Overflow, www.AcsiiTable,com and a YouTube video by OpenJavaScript.
+
+
